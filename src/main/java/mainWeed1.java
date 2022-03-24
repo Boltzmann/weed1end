2f49d9010230542636e99b229d40ffcbd8d4be12
Katharina Winkler <kasowi@outlook.com>
2022-03-24 17:14:03 +0100

    Scanner added (name input)

--- src/main/java/mainWeed1.java
+++ src/main/java/mainWeed1.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 public class mainWeed1 {
+
     public static void main(String[] args) {
 
         System.out.println(sayName("Marlin"));
         System.out.println("Hallo "+nameL00p("Stefan")+"!");
         System.out.println(greaterZero(3));
         System.out.println(greaterZero(-1));
+        weedAntGreet();
     }
 
     public static String sayName(String name) {
@@ -30,5 +33,13 @@ public class mainWeed1 {
 
     public static int square(int number) {
         return number *= number;
+    }
+
+    public static void weedAntGreet() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gib deinen Namen ein und drücke ENTER:");
+        String input = scanner.nextLine();
+
+        System.out.println(sayName(input));
     }
 }
